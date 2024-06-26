a9850f7a1d7380710ade40f94daf7b4a7d22d004
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2024-06-26 17:23:18 +0000

    added user && implemented authentication

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(protected  UserPasswordHasherInterface $hasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+         $user = new User();
+         $user->setName('Emmanuel Wilson');
+         $user->setEmail('hagioswilson@gmail.com');
+         $password = $this->hasher->hashPassword($user, 'password');
+         $user->setPassword($password);
+         $manager->persist($user);
+
+        $manager->flush();
+    }
+}
